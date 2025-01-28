252731eef96df505426b47c6e41f3a33e16dfe20
BrianDunn1432 <brian.dunn1432@gmail.com>
2025-01-27 23:48:57 -0500

    slight changes

--- xkl1s/gmgn.py
+++ xkl1s/gmgn.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any, Dict, List
 from curl_cffi import requests as c_requests
 
@@ -23,28 +24,31 @@ class GMGNWalletData:
         self.data = ret
 
     def get_holding_time(self) -> float:
-        if self.data is None:
+        if self.data == {}:
             self._pull_data()
 
         return self.data["data"]["avg_holding_peroid"]
 
     def get_win_rate(self) -> float:
-        if self.data is None:
+        if self.data == {}:
             self._pull_data()
 
-        return self.data["data"]["winrate"]
+        wr = self.data["data"]["winrate"]
+        if wr is None:
+            return 0
+        return wr
 
     def get_30d_pctchange(self) -> float:
-        if self.data is None:
+        if self.data == {}:
             self._pull_data()
 
         return self.data["data"]["pnl_30d"]
 
     def average_trades_per_day(self) -> float:
-        if self.data is None:
+        if self.data == {}:
             self._pull_data()
 
-        return (self.data["data"]["buy_30d"] + self.data["data"]["sell_30d"]) / 30
+        return (self.data["data"].get("buy_30d", 0) + self.data["data"].get("sell_30d", 0)) / 30
 
     def get_wallet_score(self) -> float:
         cur_score = 0
@@ -108,14 +112,37 @@ class GMGNTokenData:
             self.top_wallets = [GMGNWalletData(wallet["address"]) for wallet in self.top_holder_json["data"]]
         return self.top_wallets
 
-    def get_top_holder_average_holding_time(self, num_wallets: int = 4):
+    def get_top_holder_average_holding_time(self, num_wallets: int = 4) -> float:
         wallets = self.get_top_wallets()[:num_wallets]
         holding_times = [wallet.get_holding_time() for wallet in wallets]
 
-        return holding_times
+        filtered_holding_times = [time for time in holding_times if time is not None and time > 0]
+
+        if filtered_holding_times:
+            average_holding_time = sum(filtered_holding_times) / len(filtered_holding_times)
+            return average_holding_time
+        else:
+            return 0
+
+    def get_average_wallet_score(self, num_wallets: int = 4) -> float:
+        wallets = self.get_top_wallets()[:num_wallets]
+        scores = [wallet.get_wallet_score() for wallet in wallets]
+
+        filtered_scores = [time for time in scores if time is not None and time > 0]
+
+        if filtered_scores:
+            average_score = sum(scores) / len(filtered_scores)
+            return average_score
+        else:
+            return 0
 
 
 if __name__ == "__main__":
     data = GMGNTokenData("8FqXr6dw5NHA2TtwFeDz6q9p7y9uWyoEdZmpXqqUpump")
+    print(f"Top 10 avg holding: {data.get_top_holder_average_holding_time(10)}")
+    print(f"Top 10 avg score: {data.get_average_wallet_score(10)}s")
     wallet = GMGNWalletData("BJVHxVNDbBVx6Nv1zckuPyKuboy8xjvvoTRYr97pToie")
     print(wallet.get_wallet_score())
+
+    with open("data.json", "w") as f:
+        json.dump(wallet.data, f, indent=4)
