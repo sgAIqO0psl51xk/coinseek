ce27ae49d302c4a6bce70a29d552c0bda8686ce6
vineth6 <vineth661@gmail.com>
2025-01-31 17:45:14 -0500

    navbar moved

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -46,7 +46,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
