a7b9b51bc42c25d6ac8734395b54cce5b52afe56
BrianDunn1432 <brian.dunn1432@gmail.com>
2025-01-31 17:04:31 -0500

    env change

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     return config;
   },
   env: {
-    BACKEND_URL: process.env.BACKEND_URL,
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
 };
 
